89a175b12eff3c65a5daf35eb138d33d11a46295
Collin Huber <collin722999@gmail.com>
2024-02-07 17:14:58 -0500

    update files

--- app/components/input.tsx
+++ app/components/input.tsx
@@ -1,6 +1,7 @@
 "use client"
 import { useState, useEffect } from "react";
 import Solver from "./solver";
+import { randomInt } from "crypto";
 
 export default function Input() {
 
@@ -16,38 +17,137 @@ export default function Input() {
         };
     }
 
+
     useEffect(() => {
         setHasDuplicates(checkForDuplicates(inputs));
     }, [inputs]);
 
+    //
+    // const checkForDuplicates = (matrix: any[][]): boolean => {
+
+    //     let duplicates = new Set();
+    //     console.log("DUPE", duplicates)
+
+    //     for (let i = 0; i < 9; i++) {
+    //         let boxMap = new Map();
+    //         for (let j = 0; j < 9; j++) {
+    //             const boxVal = matrix[i][j];
+    //             const key = `${i}-${j}`;
+    //             if (boxVal !== '') {
+    //                 if (boxMap.has(boxVal)) {
+    //                     duplicates.add(key);
+    //                     let originalKeys = boxMap.get(boxVal);
+    //                     originalKeys.forEach((originalKey: string) => {
+    //                         duplicates.add(originalKey);
+    //                     });
+    //                     originalKeys.push(key);
+    //                     boxMap.set(boxVal, originalKeys);
+    //                 } else {
+    //                     boxMap.set(boxVal, [key]);
+    //                 }
+    //             }
+    //         }
+    //     }
+
+    //     for (let i = 0; i < 3; i++) {
+    //         for (let j = 0; j < 3; j++) {
+    //             if (isDuplicateInCol(matrix, i, j)) return true;
+    //         }
+    //     }
+
+    //     for (let i = 0; i < 9; i += 3) {
+    //         for (let j = 0; j < 9; j += 3) {
+    //             if (isDuplicateInRow(matrix, i, j)) return true;
+    //         }
+    //     }
+        
+    //     return false;
+    // }
+
+    // const isDuplicateInCol = (matrix: any[][], startRow: number, startCol: number): Object => {
+    //     let duplicates = new Set();
+    //     console.log("YUT",duplicates)
+    //     for (let i = startRow; i < 9; i+=3) {
+    //         let colMap = new Map();
+    //         for (let j = startCol; j < 9; j+=3) {
+    //             const colVal = matrix[i][j];
+    //             const key = `${i}-${j}`;
+    //             if (colVal !== '') {
+    //                 if (colMap.has(colVal)) {
+    //                     duplicates.add(key);
+    //                     let originalKeys = colMap.get(colVal);
+    //                     originalKeys.forEach((originalKey: string) => {
+    //                         duplicates.add(originalKey);
+    //                     });
+    //                     originalKeys.push(key);
+    //                     colMap.set(colVal, originalKeys);
+    //                 } else {
+    //                     colMap.set(colVal, [key]);
+    //                 }
+    //             }                
+    //         }
+    //     }
+    //     return duplicates;
+    // }
+
+    // const isDuplicateInRow = (matrix: any[][], startRow: number, startCol: number): boolean => {
+    //     let rowSet = new Set();
+    //     for (let i = startRow; i < startRow + 3; i++) {
+    //         for (let j = startCol; j < startCol + 3; j++) {
+    //             const rowVal = matrix[i][j];
+    //             if (rowVal !== '' && rowSet.has(rowVal)) return true;
+    //             rowSet.add(rowVal);
+    //         }
+    //     }
+    //     return false;
+    // }
+    //
+
     const checkForDuplicates = (matrix: any[][]): boolean => {
+
         for (let i = 0; i < 9; i++) {
-            let rowSet = new Set();
             let boxSet = new Set();
             for (let j = 0; j < 9; j++) {
-                const rowVal = matrix[i][j];
-                const boxVal = matrix[j][i];
-                if (rowVal !== '' && rowSet.has(rowVal)) return true;
+                const boxVal = matrix[i][j];
                 if (boxVal !== '' && boxSet.has(boxVal)) return true;
-                rowSet.add(rowVal);
                 boxSet.add(boxVal);
             }
-            for (let row = 0; row < 9; row += 3) {
-                for (let col = 0; col < 9; col += 3) {
-                    if (isDuplicateInSquare(matrix, row, col)) return true;
-                }
+        }
+
+        for (let i = 0; i < 3; i++) {
+            for (let j = 0; j < 3; j++) {
+                if (isDuplicateInCol(matrix, i, j)) return true;
+            }
+        }
+
+        for (let i = 0; i < 9; i += 3) {
+            for (let j = 0; j < 9; j += 3) {
+                if (isDuplicateInRow(matrix, i, j)) return true;
+            }
+        }
+        
+        return false;
+    }
+
+    const isDuplicateInCol = (matrix: any[][], startRow: number, startCol: number): boolean => {
+        let colSet = new Set();
+        for (let i = startRow; i < 9; i+=3) {
+            for (let j = startCol; j < 9; j+=3) {
+                const rowVal = matrix[i][j];
+                if (rowVal !== '' && colSet.has(rowVal)) return true;
+                colSet.add(rowVal);
             }
         }
         return false;
     }
 
-    const isDuplicateInSquare = (matrix: any[][], startRow: number, startCol: number): boolean => {
-        let squareSet = new Set<string>();
+    const isDuplicateInRow = (matrix: any[][], startRow: number, startCol: number): boolean => {
+        let rowSet = new Set();
         for (let i = startRow; i < startRow + 3; i++) {
             for (let j = startCol; j < startCol + 3; j++) {
-                const val = matrix[i][j];
-                if (val !== '' && squareSet.has(val)) return true;
-                squareSet.add(val);
+                const rowVal = matrix[i][j];
+                if (rowVal !== '' && rowSet.has(rowVal)) return true;
+                rowSet.add(rowVal);
             }
         }
         return false;
@@ -66,11 +166,11 @@ export default function Input() {
             {!submit ? <div className="grid-container">
                 {inputs.map((row, rowIndex) => (
                     <div key={rowIndex} className="grid-container">
-                        {row.map((cell, colIndex) =>
+                        {row.map((col, colIndex) =>
                             <div key={colIndex} className="grid-small-border">
                                 <input 
                                     className="grid-item"
-                                    value={cell}
+                                    value={col}
                                     onChange={(e) => handleInput(rowIndex, colIndex, e.target.value)}
                                 />
                             </div>
